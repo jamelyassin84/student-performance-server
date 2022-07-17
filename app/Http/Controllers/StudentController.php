f048c0348e1da5a68d062a49bc83f35fd19e2dbc
Jamel Yassin <jamelyassin84@gmail.com>
2022-07-17 18:57:29 +0400

    Student Actions

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -12,15 +12,9 @@ use Illuminate\Support\Facades\Hash;
 
 class StudentController extends Controller
 {
-
-    public function __construct()
-    {
-        $this->middleware('auth:sanctum')->except(['index', 'show']);
-    }
-
     public function index()
     {
-        return Student::all();
+        return User::with('student')->get();
     }
 
     public function store(StoreStudentRequest $request)
@@ -63,14 +57,12 @@ class StudentController extends Controller
         $user->student = $student;
 
         return $user;
-
-        // $data =  Student::find($student->id);
-
-        // $data->fill($request)->save();
     }
 
-    public function destroy(Student $student)
+    public function destroy($id)
     {
-        return Student::find($student->id)->delete();
+        User::find($id)->delete();
+
+        return Student::where('user_id', $id)->delete();
     }
 }
